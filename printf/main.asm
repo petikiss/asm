28003e9346ef571e4d893d56dc66190c624ed1f9
marci <marci@work>
2015-04-20 18:10:45 +0200

    stack handled correctly - no crash

---
+++ printf/main.asm
@@ -0,0 +1,26 @@
+
+extern printf
+
+section .data:
+    msg dd 0x41
+    fmt db "number=%x",10,0
+
+global main
+section .text:
+   main:
+
+
+   push ebp
+   mov ebp, esp
+
+    push dword [msg]
+    push dword fmt
+    call printf
+
+
+   mov esp, ebp
+   pop ebp
+
+   mov eax, 0 ; exit
+   ret
+
