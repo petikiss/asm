223526eec9e28bccb66331d25cdb772e1db9b362
vacak <you@example.com>
2020-04-29 15:55:56 +0200

    fixing printing out local variable

--- asm_basics/basic_operations/local_variables/local.asm
+++ asm_basics/basic_operations/local_variables/local.asm
@@ -1,14 +1,18 @@
 ; printing out a local variable value to the standard output
 
+; nasm -felf64 -g local.asm
+; ld -g local.o
+; gdb a.out, b _start, run, info frame, nexti, x/12xg $rsp
 
 
 section .text
 global _start
 _start:
-    push rbp        
+    push rbp
     mov rbp, rsp
-    mov DWORD [rbp-4], 5
-
+    ; mov DWORD [rbp-4], 3 ; NOTE: 3 is decimal number, which is ENQ in ASCII, but it is non printable!!!! 
+    mov DWORD [rbp-4], "3" ; "3" is printable. it is equal to 51 dec
+    ; mov DWORD [rbp-4], 51 ; this will print out number 3
 
 
 
@@ -20,7 +24,6 @@ _start:
     syscall
 
     pop rbp
-
     mov rax, 60 ; exit
     mov rdi, 0
     syscall
