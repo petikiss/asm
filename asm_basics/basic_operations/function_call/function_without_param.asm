bff9b207df8bf8b5cbcb2cdcf202632455bde4f6
vacak <you@example.com>
2020-04-30 12:15:33 +0200

    Function call without parameters and wihtout return value

--- asm_basics/basic_operations/function_call/function_without_param.asm
+++ asm_basics/basic_operations/function_call/function_without_param.asm
@@ -1,42 +1,36 @@
-; call = push(RIP) + JMP
-; ret = pop(RIP) + JMP
+; Function tutorial
+; - function call
+; - no return value
+; - no parameters
 
-global main
+; Descrioption: 
+; Create a function which write out "Hello function" to std output
 
-section .data
-    msg db "Hello", 10
-    msglen equ $ - msg
 
-section .text
-main:
-
-    push 2  ; second parameter of the function1
-    push 1  ; first parameter of the function2
-    call function1  ; call -> push the RIP to stack and JMP (RIP: contains the next instruction)
-    mov rbx, rax    ; rax contains the return value -> save it
-
-    mov rax, 1 ; write "Hello" to stdout
+function:
+    mov rax, 1      ; sys_write
     mov rdi, 1
     mov rsi, msg
     mov rdx, msglen
     syscall
+    ret
 
-    mov rax, 60 ; sys_exit
+section .data
+    msg db "Hello function", 10 ; dec 10 is for end line
+    msglen equ $ - msg
+
+
+section .text
+global _start
+_start:
+    push rbp        ; saving rbp
+    mov rbp, rsp    ; moving the rpb to beginning of the stack (esp)
+    
+    call function   ; push RIP to stack and JMP to function label
+
+    mov eax, 0
+    pop rbp
+    
+    mov rax, 60     ; sys_exit
     mov rdi, 0
     syscall
-
-
-; int function(int param1, int param2)
-function1: 
-        push    rbp      ; save the current base (frame) pointer register
-        mov     rbp, rsp ; <- HERE it is, where our locals starts
-
-        sub     rsp, 0x40        ; 64 bytes of local stack space 
-        mov     rbx, [rbp+8]     ; first parameter to function 
-
-        ; some more code 
-
-        mov rsp, rbp    ; leave <-> mov rsp,rbp / pop rbp
-        pop rbp         ; restore the "old" ebp
-        
-        ret             ; pop the value from the stack (RIP pushed by call) and JMP
